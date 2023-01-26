bfa4f184c97bd9367da7c515510f1133428061e3
meyersw23562 <55461160+meyersw23562@users.noreply.github.com>
2023-01-26 04:00:47 -0500

    a few more disabled classes because there's still more clutter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay2901/Autonomous/TankDriveSwerve/TankDriveSwerveLeftHighAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PowerPlay2901/Autonomous/TankDriveSwerve/TankDriveSwerveLeftHighAuto.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.PowerPlay2901.Autonomous.TankDriveSwerve;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+@Disabled
 @Autonomous(name = "Dwayne Left High Tank Drive Auto")
 public class TankDriveSwerveLeftHighAuto extends TankDriveSwerveBaseAuto{
 
